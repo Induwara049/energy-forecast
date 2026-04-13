cd5f4029d57592536d2c2a6c530575c2116a0701
Induwara <induwarajayasinghe049@gmail.com>
2026-04-13 14:07:42 +0530

    updated config with new directories

--- src/config.py
+++ src/config.py
@@ -7,13 +7,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Data paths
 DATA_DIR = BASE_DIR / "dataset"
-RAW_DATA_FILE = DATA_DIR / "energy_data_set.csv"
+RAW_DATA = DATA_DIR / "raw data"
+PROCESSED_DATA = DATA_DIR / "processed_data"
+RAW_DATA_FILE = RAW_DATA / "energy_data_set.csv"
 
 # Output paths
 OUTPUT_DIR = BASE_DIR / "outputs"
 MODEL_DIR = OUTPUT_DIR / "models"
 MODEL_FIG_DIR = OUTPUT_DIR / "model_plots"
 EDA_FIG_DIR = OUTPUT_DIR / "eda_plots"
+MODEL_RESULTS = OUTPUT_DIR / "model_results"
 
 # Ensure output directories exist
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
